70af7f16a64c6c7df380ec86cc13e13838cb0061
graceechoi <143360895+graceechoi@users.noreply.github.com>
2026-02-20 20:46:19 -0600

    Add files via upload

---
+++ website-1/script.js
@@ -0,0 +1,173 @@
+const playlists = {
+    currently: [
+        {
+            image: "img/404_kiiikiii.png",
+            title: "404 (New Era)",
+            artist: "KiiiKiii",
+            songLink: "https://open.spotify.com/track/24rDDbSlFY9OHrlJb48CRh?si=6a79004f23e740be",
+            artistLink: "https://open.spotify.com/artist/63sat2XSMKdwKfqspmYQKP?si=N7m5xd5nTnmcMxqoGj-tiw"
+        },
+
+        {
+            image: "img/hellomellow_nctwish.png",
+            title: "Hello Mellow",
+            artist: "NCT Wish",
+            songLink: "https://open.spotify.com/track/0Bta0rqkUh1ErjpwHIofP6?si=3ff4216df7414701",
+            artistLink: "https://open.spotify.com/artist/4FqmqIspLaUGtxAFFLsZxc?si=RRETH6VUQgar2Sj_cFRB9w"
+        },
+
+        {
+            image: "img/lostisland_enhypen.png",
+            title: "Lost Island",
+            artist: "Enhypen",
+            songLink: "https://open.spotify.com/track/53cWZA9OROeiYu1vHh4f4i?si=36600267a8fd4885",
+            artistLink: "https://open.spotify.com/artist/5t5FqBwTcgKTaWmfEbwQY9?si=RPq2LuFVR8Wk8gWybdrtYA"
+        }
+    ],
+
+    childhood: [
+        {
+            image: "img/cantholdus_kidzbop.png",
+            title: "Can't Hold Us",
+            artist: "Kidz Bop Kids",
+            songLink: "https://open.spotify.com/track/653eqlwbqdscFgc3ePU8Zn?si=cbd83bcf7ee84bcf",
+            artistLink: "https://open.spotify.com/artist/1Vvvx45Apu6dQqwuZQxtgW?si=RmG5_0qZQrCJFcADX-xJxg"
+        },
+
+        {
+            image: "img/fourseasons_vivaldi.png",
+            title: "The Four Seasons: 'Winter'",
+            artist: "Vivaldi",
+            songLink: "https://open.spotify.com/track/0ON4FYmS4Zch1NV0lhv9hX?si=33c80b3add264dac",
+            artistLink: "https://open.spotify.com/artist/2QOIawHpSlOwXDvSqQ9YJR?si=DuHZCJ87Q9yMpiyoUvL2uw"
+        }
+    ],
+
+    middleschool: [
+        {
+            image: "img/cakebytheocean_dnce.png",
+            title: "Cake By The Ocean",
+            artist: "DNCE",
+            songLink: "https://open.spotify.com/track/3R4jodloIlJ3GQffZtxlDI?si=0a1df1140c1c4d89",
+            artistLink: "https://open.spotify.com/artist/6T5tfhQCknKG4UnH90qGnz?si=ec4995cb45a84eab"
+        },
+
+        {
+            image: "img/sugar_maroon5.png",
+            title: "Sugar",
+            artist: "Maroon 5",
+            songLink: "https://open.spotify.com/track/2iuZJX9X9P0GKaE93xcPjk?si=c7880f916e6a426c",
+            artistLink: "https://open.spotify.com/artist/04gDigrS5kc9YWfZHwBETP?si=8ddb1803fdf24895"
+        },
+
+        {
+            image: "img/gee_girlsgen.png",
+            title: "Gee",
+            artist: "Girls' Generation",
+            songLink: "https://open.spotify.com/track/2BQIMF7CyLe2xAKzh74A4C?si=c67a006d8c6f4638",
+            artistLink: "https://open.spotify.com/artist/0Sadg1vgvaPqGTOjxu0N6c?si=a6b8a193103241ba"
+        },
+    ],
+
+    highschool: [
+        {
+            image: "img/bloodsweattears_bts.png",
+            title: "Blood Sweat & Tears",
+            artist: "BTS",
+            songLink: "https://open.spotify.com/track/2u54HNQamwFuOMLSuhSRom?si=c03898e503b94a56",
+            artistLink: "https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX?si=f435e4733bca4ec0"
+        },
+
+        {
+            image: "img/clap_svt.png",
+            title: "CLAP",
+            artist: "Seventeen",
+            songLink: "https://open.spotify.com/track/19t5GSN3XsLB7UOsZD8Fwv?si=08a37b7e97db44fc",
+            artistLink: "https://open.spotify.com/artist/7nqOGRxlXj7N2JYbgNEjYH?si=321cdce338d7470d"
+        },
+
+        {
+            image: "img/beaubeau_onf.png",
+            title: "Beautiful Beautiful",
+            artist: "ONF",
+            songLink: "https://open.spotify.com/track/79QAI89acpSdemBXCNFJzr?si=ffac21479fa94848",
+            artistLink: "https://open.spotify.com/artist/0eEhOgZ2x6kv8kLz77WO7b?si=ec9c15069b7c47b8"
+        },
+    ],
+
+    college: [
+        {
+            image: "img/knowhow_lucy.png",
+            title: "Knowhow",
+            artist: "LUCY",
+            songLink: "https://open.spotify.com/track/0LB2efsySRVfw2lvBekR98?si=f647fd3fe53940e1",
+            artistLink: "https://open.spotify.com/artist/4eh2JeBpQaScfHKKXZh5vO?si=74dfe219aa7d4bbc"
+        },
+
+        {
+            image: "img/supershy_newjeans.png",
+            title: "Super Shy",
+            artist: "New Jeans",
+            songLink: "https://open.spotify.com/track/5sdQOyqq2IDhvmx2lHOpwd?si=f1e501a0cf4644bc",
+            artistLink: "https://open.spotify.com/artist/6HvZYsbFfjnjFrWF950C9d?si=ef42abc49fe541ef"
+        },
+
+        {
+            image: "img/plottwist_tws.png",
+            title: "plot twist",
+            artist: "TWS",
+            songLink: "https://open.spotify.com/track/0aZG8KWrpRnsGL0loUkfSj?si=3df0f65343f44e0b",
+            artistLink: "https://open.spotify.com/artist/4GgBKgxhc649frZDHcXIEz?si=18791f52909640ed"
+        },
+
+        {
+            image: "img/ifisayily_boynextdoor.png",
+            title: "IF I SAY, I LOVE YOU",
+            artist: "BOYNEXTDOOR",
+            songLink: "https://open.spotify.com/track/7GOIrZTegzVty8mhNhRuA0?si=b6a977a4bf1342e9",
+            artistLink: "https://open.spotify.com/artist/4hnHLgMSOiqERWBL4jINP1?si=44e782957f3444b8"
+        }
+    ]
+};
+
+const players = document.querySelectorAll(".music-player-small, .music-player-full");
+
+players.forEach(player => {
+
+    const era = player.dataset.era;
+    const songs = playlists[era];
+
+    let currentIndex = 0;
+
+    const songElement = player.querySelector(".song");
+    const artistElement = player.querySelector(".artist");
+    const nextButton = player.querySelector(".next");
+    const prevButton = player.querySelector(".prev");
+    const playButton = player.querySelector(".play");
+    const imageElement = player.querySelector("img");
+
+    function updatePlayer() {
+        const currentSong = songs[currentIndex];
+
+        songElement.textContent = currentSong.title;
+        songElement.href = currentSong.songLink;
+        playButton.href = currentSong.songLink;
+
+        artistElement.textContent = currentSong.artist;
+        artistElement.href = currentSong.artistLink;
+
+        imageElement.src = currentSong.image;
+    }
+
+    nextButton.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % songs.length;
+        updatePlayer();
+    });
+
+    prevButton.addEventListener("click", () => {
+        currentIndex = (currentIndex - 1 + songs.length) % songs.length;
+        updatePlayer();
+    });
+
+    updatePlayer();
+});
